30aa4d55c70a16dcb79d82bd4c86a020e4ae8d71
jeremy <94247773+1jehuang@users.noreply.github.com>
2026-02-21 01:15:35 -0800

    Bump extension to 0.9.3, update AMO uuid, disable fingerprint requirement

--- native-host-wrapper.sh
+++ native-host-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export FAB_AUTOLOGIN_REQUIRE_FINGERPRINT=true
+export FAB_AUTOLOGIN_REQUIRE_FINGERPRINT=false
 exec /home/jeremy/firefox-agent-bridge/rust-cli/target/release/firefox-agent-bridge-host "$@"
